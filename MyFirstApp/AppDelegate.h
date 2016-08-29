d089efea54f4ded095076794c984a02ba5537736
techspectrum <technologyspectrum2013@gmail.com>
2016-08-29 19:09:06 +0530

    First Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Anand Patel on 29/08/16.
+//  Copyright © 2016 Anand Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by Anand Patel on 29/08/16.
+//  Copyright © 2016 Anand Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
